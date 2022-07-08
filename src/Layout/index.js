113700282681ba7e0fb27e5b91195d6723edebd7
Eddy Rios <Eddyrios101@gmail.com>
2022-07-08 14:56:33 -0700

    Drastic changes due to severe bug in CreateDeck

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,58 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
+import { Switch, Route } from 'react-router-dom';
 import Header from "./Header";
 import NotFound from "./NotFound";
-import CreateDeck from "./CreateDeck";
-import EditDeck from "./EditDeck";
-import EditCard from "./EditCard";
-import AddCard from "./AddCard";
-import Home from "./Home";
-import Study from "./Study";
-import DeckList from "./DeckList";
-import ViewDeck from "./ViewDeck";
-import {
-  Route,
-  Switch,
-} from "react-router-dom";
+import Home from "./home/Home";
+import CreateDeck from "./Deck/DeckForms/CreateDeck";
+import DeckRoutes from "./Deck/DeckRoutes";
 
 function Layout() {
-  //DeckList => listDecks
-  const [decks, setDecks] = useState([]);
-  //Study => readDeck - loads deck and cards
-  const [deck, setDeck] = useState({});
-  //AddCard, editCard, => createCard, updateCard
-  const [card, setCard] = useState([]);
-
   return (
     <>
-    <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home decks={decks} setDecks={setDecks}/>
-          <DeckList decks={decks} setDecks={setDecks}/>
-        </Route>
-        <Route exact path="/decks/:deckId/study">
-          <Study deck={deck} setDeck={setDeck}/>
-        </Route>
-        <Route exact path="/decks/new">
-          <CreateDeck />
-        </Route>
-        <Route exact path="/decks/:deckId">
-          <ViewDeck />
-        </Route>
-        <Route exact path="/decks/:deckId/edit">
-          <EditDeck deck={deck} setDeck={setDeck}/>
-        </Route>
-        <Route exact path="/decks/:deckId/cards/new">
-          <AddCard deck={deck} setDeck={setDeck} card={card} setCard={setCard}/>
-        </Route>
-        <Route exact path="/decks/:deckId/cards/:cardId/edit">
-          <EditCard deck={deck} setDeck={setDeck} card={card} setCard={setCard} />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-</>
+      <Header />
+      <div className="container">
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/decks/new'>
+            <CreateDeck />
+          </Route>
+          <Route path='/decks/:deckId'>
+            <DeckRoutes />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+        
+      </div>
+    </>
   );
 }
 
